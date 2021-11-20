baac05f3404f9ba21c02126ac25fb958c3037504
Maya Borshcheva <maya7maks@gmail.com>
2021-11-20 22:01:03 +0200

    added bufferReader, bufferWriter into client,server

--- src/main/java/com/borshcheva/echoserver/Client.java
+++ src/main/java/com/borshcheva/echoserver/Client.java
@@ -1,27 +1,28 @@
 package com.borshcheva.echoserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("localhost", 3000);
-        Scanner scanner = new Scanner(System.in);
+        try (Socket socket = new Socket("localhost", 3000);
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        ) {
+            while (true) {
+                Scanner scanner = new Scanner(System.in);
+                String message = scanner.nextLine();
 
-        String message = scanner.nextLine();
+                bufferedWriter.write(message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
 
-        OutputStream outputStream = socket.getOutputStream();
-        outputStream.write(message.getBytes());
+                String echo = bufferedReader.readLine();
+                System.out.println(echo);
+            }
 
-        InputStream inputStream = socket.getInputStream();
-        byte[] buffer = new byte[100];
-        int count = inputStream.read(buffer);
-        System.out.println(new String(buffer, 0, count));
+        }
 
-        inputStream.close();
-        outputStream.close();
     }
 }
--- src/main/java/com/borshcheva/echoserver/Server.java
+++ src/main/java/com/borshcheva/echoserver/Server.java
@@ -1,30 +1,22 @@
 package com.borshcheva.echoserver;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(3000);
+             Socket socket = serverSocket.accept();
+             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));) {
 
-        ServerSocket serverSocket = new ServerSocket(3000);
-        while (true) {
-            Socket socket = serverSocket.accept();
-            InputStream inputStream = socket.getInputStream();
-            byte[] buffer = new byte[100];
-            int count = inputStream.read(buffer);
-            String message = new String(buffer, 0, count);
-            message = "echo " + message;
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(message.getBytes());
-
-            inputStream.close();
-            outputStream.close();
-
+            while (true) {
+                String messageFromClient = bufferedReader.readLine();
+                bufferedWriter.write("echo: " + messageFromClient);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
         }
-
-
     }
 }
